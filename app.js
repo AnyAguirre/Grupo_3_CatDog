6b88bf69b5898110b07db6829a83e8654ce6ae95
Analia Aguirre <90473382+AnyAguirre@users.noreply.github.com>
2022-03-16 17:39:09 -0300

    Cargando HOME

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = 3030;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/views/index.html'))
+})
+
+app.listen(PORT, () => console.log(`
+Servidor escuchando en el puerto ${PORT}
+http://localhost:${PORT}
+`))
