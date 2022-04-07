ecd6328e4e4459110ec69019b103b9e65b4b8247
Daniel Mamani <danielmamani18.07@gmail.com>
2022-04-07 20:53:10 -0300

    un mini cambio de app.js

--- src/app.js
+++ src/app.js
@@ -5,22 +5,22 @@ const PORT = 3030;
 
 app.use(express.static('public'));
 
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 
 /*Enrutadores*/
 const indexRouter = require('./routes/indexRouter');
 const carritoRouter = require('./routes/carritoRouter')
-const productosRouter = require('./routes/productosRouter')
-const userRouter = require('./routes/userRouter')
+const productosRouter = require('./routes/productosRouter');
+const userRouter = require('./routes/userRouter');
 
 
 /*Rutas*/
 app.use('/', indexRouter);
 app.use('/carrito', carritoRouter);
-app.use('/producto', productosRouter)
-app.use('/user', userRouter)
+app.use('/producto', productosRouter);
+app.use('/user', userRouter);
 
 app.listen(PORT, () => console.log(`
 Servidor escuchando en el puerto ${PORT}
