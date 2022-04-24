f55c79b9e832aec0fb88414033fb85eb595b3220
Daniel Mamani <danielmamani18.07@gmail.com>
2022-04-24 18:57:22 -0300

    Creacion de CRUD de productos

---
+++ src/routes/adminRouter.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.router;
+
+
+/* CRUD DE PRODUCTOS */
+
+/* GET - LISTADO DE PRODUCTOS */
+router.get('/productos', )
+/* GET - CREACION DE PRODUCTO */
+router.get('/productos/crear', )
+/* GET - DETALLE DE PRODUCTO */
+router.get('/productos/:id', )
+/* POST - CREAR PRODUCTO */
+router.post('/productos', )
+/* GET - EDITAR PRODUCTO */
+router.get('/productos/editar/:id', )
+/* PUT - ACTUALIZAR PRODUCTO */
+router.put('/productos/:id', )
+/* DELETE - ELIMIAR UN PRODUCTO */
+router.delete('/productos/eliminar/:id', )
