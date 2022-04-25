9d5a04b4e763214fc0421deb91cfe64989e76424
Daniel Mamani <danielmamani18.07@gmail.com>
2022-04-25 20:47:39 -0300

    Modiicando CRUD

--- src/routes/adminRouter.js
+++ src/routes/adminRouter.js
@@ -1,20 +1,22 @@
 const express = require('express');
-const router = express.router;
-
+const router = express.Router( );
+const adminProductsController = require('../controllers/admin/adminProductsController');
 
 /* CRUD DE PRODUCTOS */
 
 /* GET - LISTADO DE PRODUCTOS */
-router.get('/productos', )
+router.get('/productos', adminProductsController);
 /* GET - CREACION DE PRODUCTO */
-router.get('/productos/crear', )
+router.get('/productos/crear', adminProductsController);
 /* GET - DETALLE DE PRODUCTO */
-router.get('/productos/:id', )
+router.get('/productos/:id', adminProductsController);
 /* POST - CREAR PRODUCTO */
-router.post('/productos', )
+router.post('/productos', adminProductsController);
 /* GET - EDITAR PRODUCTO */
-router.get('/productos/editar/:id', )
+router.get('/productos/editar/:id', adminProductsController);
 /* PUT - ACTUALIZAR PRODUCTO */
-router.put('/productos/:id', )
+router.put('/productos/:id', adminProductsController);
 /* DELETE - ELIMIAR UN PRODUCTO */
-router.delete('/productos/eliminar/:id', )
+router.delete('/productos/eliminar/:id', adminProductsController);
+
+module.exports = router;
