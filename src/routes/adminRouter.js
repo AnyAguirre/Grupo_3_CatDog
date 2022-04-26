a07237c1c5ec419ae18ab4bfdaa34a0e677e0127
Daniel Mamani <danielmamani18.07@gmail.com>
2022-04-25 21:59:53 -0300

    modificacion de CRUD de Productos

--- src/routes/adminRouter.js
+++ src/routes/adminRouter.js
@@ -7,11 +7,11 @@ const adminProductsController = require('../controllers/admin/adminProductsContr
 /* GET - LISTADO DE PRODUCTOS */
 router.get('/productos', adminProductsController.list);
 /* GET - CREACION DE PRODUCTO */
-router.get('/productos/crear', adminProductsController.productCreate);
+router.get('/productos/create', adminProductsController.productCreate);
 /* GET - DETALLE DE PRODUCTO */
 router.get('/productos/:id', adminProductsController.detail);
 /* POST - CREAR PRODUCTO */
-router.post('/productos', adminProductsController.productCreate);
+router.post('/productos', adminProductsController.productAdd);
 /* GET - EDITAR PRODUCTO */
 router.get('/productos/editar/:id', adminProductsController.productEdit);
 /* PUT - ACTUALIZAR PRODUCTO */
